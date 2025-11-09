60054ba80dd1ad7e41c1abd3736a44b97c7d23dc
VhBuriti <victorhugoburit@hotmail.com>
2025-11-08 22:55:43 -0300

    chore: breadcrumb formatting

--- frontend/src/components/molecules/Breadcrumb/index.tsx
+++ frontend/src/components/molecules/Breadcrumb/index.tsx
@@ -1,27 +1,27 @@
-import styles from './index.module.scss';
+import styles from "./index.module.scss";
 
 export default function Breadcrumb() {
-  let currentLink = '';
+  let currentLink = "";
   const { pathname } = window.location;
-  const pathParts = pathname.split('/').filter(Boolean);
+  const pathParts = pathname.split("/").filter(Boolean);
 
   const crumbs = pathParts.map((crumb) => {
     currentLink += `/${crumb}`;
     const isCurrent = currentLink === pathname;
     const label = decodeURIComponent(crumb)
-      .replace(/-/g, ' ')
+      .replace(/-/g, " ")
       .replace(/\b\w/g, (c) => c.toUpperCase());
 
     return (
       <li
         key={currentLink}
         data-breadcrumb-item
-        data-current={isCurrent ? 'true' : 'false'}
+        data-current={isCurrent ? "true" : "false"}
       >
         <a
           href={currentLink}
           data-breadcrumb-link
-          aria-current={isCurrent ? 'page' : undefined}
+          aria-current={isCurrent ? "page" : undefined}
         >
           {label}
         </a>
@@ -34,12 +34,12 @@ export default function Breadcrumb() {
       <ul data-breadcrumb-list>
         <li
           data-breadcrumb-item
-          data-current={pathname === '/' ? 'true' : 'false'}
+          data-current={pathname === "/" ? "true" : "false"}
         >
           <a
             href="/"
             data-breadcrumb-link
-            aria-current={pathname === '/' ? 'page' : undefined}
+            aria-current={pathname === "/" ? "page" : undefined}
           >
             Home
           </a>
